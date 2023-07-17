e479fb9e5085636576d79668175a4d02642c619c
PJAheibam <prosenjitsingha28@gmail.com>
2023-07-17 12:51:54 +0600

    api middleware added

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,26 @@
+import { Typography, Box, Paper } from "@mui/material";
+import { useGetBooksQuery } from "../../redux/features/books/books.api";
 function HomePage() {
-  return <div>HomePage</div>;
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { data, isLoading } = useGetBooksQuery(undefined);
+
+  console.log(isLoading, data);
+
+  return (
+    <div className="p-6">
+      <Typography variant="h5" component="h1" className="font-medium">
+        Latest Published Book Books
+      </Typography>
+      <Box
+        sx={{
+          display: "grid",
+          gridTemplateColumns: `repeat(auto-fit, minmax(200px, 1fr))`,
+        }}
+      >
+        <Paper></Paper>
+      </Box>
+    </div>
+  );
 }
 
 export default HomePage;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import booksReducer from "./features/books/books.slice";
 import userReducer from "./features/user/user.slice";
+import api from "./api";
 
 const store = configureStore({
   reducer: {
     books: booksReducer,
     user: userReducer,
+    [api.reducerPath]: api.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 });
 
 export default store;
